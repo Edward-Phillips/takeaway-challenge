cda74bf1e04c07b4116ae58b10a38a5bd31e4d13
Edward Phillips <edd0ddi0@gmail.com>
2020-03-28 16:57:36 +0000

    added initial tests for Menu class

---
+++ spec/menu_item_spec.rb
@@ -0,0 +1,11 @@
+require 'menu'
+
+describe Menu do
+  let(:menu) {Menu.new}
+  context ' seeing available dishes: ' do
+    it " should be able to tell you today's menu" do
+    end
+    it ' should be able to store the specials when the menu is made' do
+    end
+  end
+end
